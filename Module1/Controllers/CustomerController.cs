f175268c40603c0905b0d7dc62073cf307255810
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-11 00:26:32 +0530

    Model Validation Test

---
+++ Module1/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module1.Models;
+
+namespace Module1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        static List<Customer> Customers = new List<Customer>()
+        {
+            new Customer(){ Id = 1, Name="Shub", Email="shub@gmail.com", Mobile="9900889908"},
+            new Customer(){Id = 2, Name="Abhi",Email="abhi@gmail.com",Mobile="9988776655"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return Customers;
+        }
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                Customers.Add(customer);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+
+    }
+}
--- Module1/Models/Customer.cs
+++ Module1/Models/Customer.cs
@@ -9,9 +9,10 @@ namespace Module1.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="Name cannot be empty"),RegularExpression("^[a-ZA-Z]+[a-zA-Z\\s]$",ErrorMessage ="Name should be alphabets only"),StringLength(100)]
+        [Required(ErrorMessage ="Name cannot be empty")]
         public string Name { get; set; }
-        [RegularExpression("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",ErrorMessage ="Email is not in valid format")]
+        //[RegularExpression("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",ErrorMessage ="Email is not in valid format")]
+        [EmailAddress(ErrorMessage ="Email format is not valid")]
         public string Email { get; set; }
         [RegularExpression("^[0-9]{10}$",ErrorMessage ="Mobile number should be in format")]
         public string Mobile { get; set; }
