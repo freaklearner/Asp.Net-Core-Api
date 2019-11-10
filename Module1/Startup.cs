ff34bb353482c34b8852a38b5093648c27535efc
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-11 01:11:01 +0530

    Action Filter Added and tested

--- Module1/Startup.cs
+++ Module1/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Module1.ActionFilters;
 
 namespace Module1
 {
@@ -25,7 +26,9 @@ namespace Module1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(config=> {
+                config.Filters.Add(new ValidateActionFilter());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
