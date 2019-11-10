912a2f7258e43355b3a9f29ca80a06c00ec9a376
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-10 23:44:08 +0530

    Model Validation Annotation Added

--- Module1/Models/Customer.cs
+++ Module1/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace Module1.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name cannot be empty"),RegularExpression("^[a-ZA-Z]+[a-zA-Z\\s]$",ErrorMessage ="Name should be alphabets only"),StringLength(100)]
+        public string Name { get; set; }
+        [RegularExpression("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",ErrorMessage ="Email is not in valid format")]
+        public string Email { get; set; }
+        [RegularExpression("^[0-9]{10}$",ErrorMessage ="Mobile number should be in format")]
+        public string Mobile { get; set; }
+
     }
 }
