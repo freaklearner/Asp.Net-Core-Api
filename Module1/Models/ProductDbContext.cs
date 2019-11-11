2496cbfcfe5f8b031321143d772cd1401aac62b2
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-12 02:36:46 +0530

    DbContext file added with its configuration in startup.cs

---
+++ Module1/Models/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Module1.Models
+{
+    public class ProductDbContext:DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+    }
+}
